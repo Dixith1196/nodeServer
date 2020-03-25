f592e2a6be49f8db213ec12fef23fd43aa91fc9c
Maram <S538365@nwmissouri.edu>
2020-03-24 20:51:25 -0500

    changes

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const http = require('http');
+  
+const http = require('http')  // require built-in Node.js http package
 
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
+// Use hosting values if available, otherwise default 
 const hostname = process.env.hostname || '0.0.0.0' // allow remote access
 const port = process.env.PORT || 3002
 
+// define our server
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('My SID is S538365');
-});
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('My name is S538365')
+})
 
+// start listening
+// use the server console to tell user where to find the server
+// use backticks for template literals with embedded expressions
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-  console.log("Lindsay really hopes this works")
-});
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
