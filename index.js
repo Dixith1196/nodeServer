411ee8d1bf8fa7faed8c17f3e6a97ec824131106
Maram <S538365@nwmissouri.edu>
2020-03-24 21:39:39 -0500

    text change

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const http = require('http')  // require built-in Node.js http package
 
 // Use hosting values if available, otherwise default 
 const hostname = process.env.hostname || '0.0.0.0' // allow remote access
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
 // define our server
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('My name is S538365')
+  res.end('My name is Deekshith Maram and my SID is S538365')
 })
 
 // start listening
